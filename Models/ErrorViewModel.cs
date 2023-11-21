b7077b282343bd25ac15121138e471ce0166e18b
tomcrane <tom@nanoplanet.com>
2023-11-21 13:20:59 +0000

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace helloworld_net.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
